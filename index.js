3cc882ae25affea661921fa41e48fcbf1099bdf9
YevaUsatova <yeva.usatova@gmail.com>
2021-06-28 23:43:59 -0400

    updates

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ const myURL= "http://localhost:3000/animals"
 fetch(myURL)
 .then(response => response.json())
 .then(data => {
-    console.log(data)
+    const ul = document.querySelector("#cute-animals");
+    data.forEach(animal => {
+        const idS=`${animal.name} ${animal.fact}`;
+        const li= document.createElement('li');
+        li.textContent = idS;
+        ul.append(li)
+    });
 })
